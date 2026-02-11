6c140374b08262163252ec0291fa6797f17fc294
Timo Wyszynski <timo.wyszynski@gmail.com>
2026-02-11 19:51:45 +0100

    Added a few constants, added a player that is a circle visualized as a  triangle, updated main() to show the player inside the game loop.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -20,6 +23,8 @@ def main():
                 return
             
         screen.fill("black")
+
+        player.draw(screen)
 
         pygame.display.flip()
 
