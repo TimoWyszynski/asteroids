ffd502d116b9b78883ddefb25a10c4ba6f0a540a
Timo Wyszynski <timo.wyszynski@gmail.com>
2026-02-11 19:38:48 +0100

    Added fps.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -18,8 +20,10 @@ def main():
                 return
             
         screen.fill("black")
-        
+
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
