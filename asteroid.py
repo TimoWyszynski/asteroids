df9e6d49d2e61566657d8f1a01496ababd9be489
Timo Wyszynski <timo.wyszynski@gmail.com>
2026-02-14 11:12:48 +0100

    Project done. The game is now finished. Asteroids split when shot.

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
 
 
 class Asteroid(CircleShape):
@@ -13,4 +15,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+
+        random_angle = random.uniform(20, 50)
+        first_velocity_vector = self.velocity.rotate(random_angle)
+        second_velocity_vector = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        first_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        second_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+
+        first_asteroid.velocity = first_velocity_vector * 1.2
+        second_asteroid.velocity = second_velocity_vector * 1.2
         
